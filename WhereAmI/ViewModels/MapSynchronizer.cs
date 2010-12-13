70127ea703a2a4602840dd21473f4ad42b3a56d2
Sascha Depold <sascha@depold.com>
2010-12-13 14:59:36 +0100

    cancel image loading if request is not finished but map should be updated with new params

--- WhereAmI/ViewModels/MapSynchronizer.cs
+++ WhereAmI/ViewModels/MapSynchronizer.cs
@@ -8,6 +8,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Phone.Controls.Maps;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace WhereAmI
         int zoom;
         bool showAlternative;
 
+        List<WebClient> clients;
+
         public MapSynchronizer() {
             lat = lon = 0;
             zoom = 12;
@@ -33,6 +36,8 @@ namespace WhereAmI
 
             maps = new Dictionary<string, object>();
             images = new Dictionary<string, object>();
+
+            clients = new List<WebClient>();
         }
 
         public int getZoom()
@@ -64,8 +69,6 @@ namespace WhereAmI
             updateMapLocations();
         }
 
-
-
         private Dictionary<String, double> getMapDimension()
         {
             Dictionary<String, double> result = new Dictionary<string,double>();
@@ -94,17 +97,15 @@ namespace WhereAmI
             switch (type)
             {
                 case "google":
-                    template = "http://maps.google.com/maps/api/staticmap?center={0},{1}&zoom={2}&size={3}x{4}&sensor=true";
+                    template = "http://maps.google.com/maps/api/staticmap?center={0},{1}&zoom={2}&size={3}x{4}&sensor=true&markers=color:blue|{0},{1}";
                     if (showAlternative) template += "&maptype=satellite";
                     break;
                 case "osm":
-                    if (showAlternative)
-                        template = "http://ojw.dev.openstreetmap.org/StaticMap/?lat={0}&lon={1}&z={2}&w={3}&h={4}&layer=cycle&show=1";
-                    else
-                        template = "http://tah.openstreetmap.org/MapOf/?lat={0}&long={1}&z={2}&w={3}&h={4}&format=png";
+                    template = "http://staticmap.openstreetmap.de/staticmap.php?center={0},{1}&zoom={2}&size={3}x{4}&markers={0},{1},red-pushpin";
+                    if (showAlternative) template += "&maptype=cycle";
                     break;
                 case "bing":
-                    template = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/{0},{1}/{2}?mapSize={3},{4}&key=" + BING_KEY;
+                    template = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/{0},{1}/{2}?mapSize={3},{4}&pushpin={0},{1};35&key=" + BING_KEY;
                     if (showAlternative) template = template.Replace("/Road/", "/Aerial/");
                     break;
             }
@@ -117,6 +118,7 @@ namespace WhereAmI
             if ((maps.Count == 0) && (images.Count == 0)) return;
 
             Dictionary<String, double> dimension = getMapDimension();
+            cancelImageLoadTasks();
 
             foreach (KeyValuePair<String, Object> pair in maps)
                 ((Map)pair.Value).SetView(new System.Device.Location.GeoCoordinate(lat, lon), zoom);
@@ -128,17 +130,30 @@ namespace WhereAmI
             if (handler != null) handler(this, EventArgs.Empty);
         }
 
+        private void cancelImageLoadTasks()
+        {
+            foreach (WebClient client in clients)
+                client.CancelAsync();
+
+            clients.Clear();
+        }
+
         private void updateImage(Image i, Uri uri)
         {
             WebClient client = new WebClient();
 
             client.OpenReadCompleted += new OpenReadCompletedEventHandler(delegate(object sender, OpenReadCompletedEventArgs e) {
-                BitmapImage imageToLoad = new BitmapImage();
-                imageToLoad.SetSource(e.Result as Stream);
-                i.Source = imageToLoad;
+                try
+                {
+                    BitmapImage imageToLoad = new BitmapImage();
+                    imageToLoad.SetSource(e.Result as Stream);
+                    i.Source = imageToLoad;
+                } catch (Exception) {
+                }
             });
 
             client.OpenReadAsync(uri, uri.AbsoluteUri);
+            clients.Add(client);
         }
     }
 }
